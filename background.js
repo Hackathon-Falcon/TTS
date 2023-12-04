5ac71c6a42fb35bd7bb32668a47c74a8962ccbbb
dongsub-joung <ehdtjqwjd@kangwon.ac.kr>
2023-12-04 23:10:58 +0900

    url

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 // VALUE
 const ZUNDAMON_VOICE= 1;
 // const BASE_URL = 'http://127.0.0.1:50021';
-const BASE_URL= `https://e1a5-218-38-158-181.ngrok-free.app `;
+const BASE_URL= `https://1c37-218-38-158-181.ngrok-free.app `;
 
 // For options
 // let selectedVolume = localStorage.getItem('selectedVolume');
